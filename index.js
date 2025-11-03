3cdfe9ee18629167ae2e5156fea9474244235ff1
morashidi <rashidi2212003@gmail.com>
2025-11-03 12:51:47 -0800

    delete unused files

--- index.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-
-mongoose.connect('mongodb://127.0.0.1:27017/testDB')
-  .then(() => console.log('✅ Connected to MongoDB successfully'))
-  .catch(err => console.error('❌ Connection failed:', err));
-
